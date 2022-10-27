c7315612346772efa7f4640b491b9085b7ac3f17
Cong Thanh <pdcthanh112.dev@gmail.com>
2022-10-27 14:26:39 +0700

    27/10/2022

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -50,12 +50,12 @@ const Dashboard = () => {
   const onHandleSearch = () => {
     setIsLoading(true)
     searchRecruimentRequest(searchObject).then((response) => {
-      console.log('res',response.data);
+      console.log('res', response.data);
       if (response.data.length > 0) {
         setListRecruitment(response.data)
         setSearchError(false)
       } else {
-        setSearchError(true)       
+        setSearchError(true)
         setPagination({ ...pagination, currentPage: 1 })
       }
       setSearchObject({ experience: "", industry: "", jobLevel: "", jobTitle: "", location: "", salary: "", typeOfWork: "" })
@@ -63,36 +63,35 @@ const Dashboard = () => {
     })
   };
 
-
   if (isLoading) return <ReactLoading className='mx-auto my-5' type='spinningBubbles' color='#bfbfbf' />
 
   return (
     <React.Fragment>
       <div className="search-container">
-
+   
         <Autocomplete
           defaultValue={''}
           options={typeOfWorkData()}
           size={'small'}
           sx={{ width: 170, marginRight: 2 }}
           renderInput={(params) => <TextField {...params} label="Loại công việc" />}
-          onInputChange={(event, value) => { handleChangeSearchObject('typeOfWork', value.value) }} />
-
+          onInputChange={(event, value) => { handleChangeSearchObject('typeOfWork', value) }} />
+          
         <Autocomplete
           defaultValue={''}
           options={jobLevelData()}
           size={'small'}
           sx={{ width: 125, marginRight: 2 }}
           renderInput={(params) => <TextField {...params} label="Vị trí" />}
-          onChange={(event, value) => { handleChangeSearchObject('jobLevel', value.value) }} />
-
+          onInputChange={(event, value) => { handleChangeSearchObject('jobLevel', value) }} />
+          
         <Autocomplete
           defaultValue={''}
           options={searchDataCategory.industry}
           size={'small'}
           sx={{ width: 170, marginRight: 2 }}
           renderInput={(params) => <TextField {...params} label="Chuyên môn" />}
-          onChange={(event, value) => { handleChangeSearchObject('industry', value) }} />
+          onInputChange={(event, value) => { handleChangeSearchObject('industry', value) }} />
 
         <Autocomplete
           defaultValue={''}
@@ -100,7 +99,7 @@ const Dashboard = () => {
           size={'small'}
           sx={{ width: 200, marginRight: 2 }}
           renderInput={(params) => <TextField {...params} label="Tên công việc" />}
-          onChange={(event, value) => { handleChangeSearchObject('jobTitle', value) }} />
+          onInputChange={(event, value) => { handleChangeSearchObject('jobTitle', value) }} />
 
         <Autocomplete
           defaultValue={''}
@@ -108,7 +107,7 @@ const Dashboard = () => {
           size={'small'}
           sx={{ width: 145, marginRight: 2 }}
           renderInput={(params) => <TextField {...params} label="Kinh nghiệm" />}
-          onChange={(event, value) => { handleChangeSearchObject('experience', value.value) }} />
+          onInputChange={(event, value) => { handleChangeSearchObject('experience', value) }} />
 
         <Autocomplete
           defaultValue={''}
@@ -116,7 +115,7 @@ const Dashboard = () => {
           size={'small'}
           sx={{ width: 200, marginRight: 2 }}
           renderInput={(params) => <TextField {...params} label="Mức lương" />}
-          onChange={(event, value) => { handleChangeSearchObject('salary', value.value) }} />
+          onInputChange={(event, value) => { handleChangeSearchObject('salary', value) }} />
 
         <Autocomplete
           defaultValue={''}
@@ -124,7 +123,7 @@ const Dashboard = () => {
           size={'small'}
           sx={{ width: 130, marginRight: 0 }}
           renderInput={(params) => <TextField {...params} label="Địa điểm" />}
-          onChange={(event, value) => { handleChangeSearchObject('location', value.value) }} />
+          onInputChange={(event, value) => { handleChangeSearchObject('location', value) }} />
 
         <button onClick={() => { onHandleSearch() }} className='btn-search bg-[#50d71e]'><i className="fa-solid fa-magnifying-glass mr-1"></i>Search</button>
         {/* <button onClick={() => { onHandleClear() }} className='btn-search bg-[#f3483b]'>Clear<i className="fa-solid fa-xmark ml-1"></i></button> */}
