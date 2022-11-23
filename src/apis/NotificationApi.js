8e5de1a970905c6123f29946511506569f5ba1dd
Cong Thanh <pdcthanh112.dev@gmail.com>
2022-11-24 00:11:58 +0700

    23/11/2022

--- src/apis/NotificationApi.js
+++ src/apis/NotificationApi.js
@@ -1,8 +1,37 @@
 import axiosConfig from "../configs/axiosConfig";
 
-export const getAllNotification = async (token, id, pageNo, pageSize) => {
-  return await axiosConfig.get(`notification/getByCandidate?candidateId=${id}&pageNo=${pageNo}&pageSize=${pageSize}`, 
-    {headers: { Authorization: `Bearer ${token}` }})
+export const getInterviewNotification = async (token, id, pageNo, pageSize) => {
+  return await axiosConfig
+    .get(
+      `interview/getInterviewByCandidateID?candidateId=${id}&pageNo=${pageNo}&pageSize=${pageSize}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
     .then((response) => response.data)
     .catch((error) => error);
+};
+
+export const confirmInterview = async (token, candidateId, interviewId) => {
+  return await axiosConfig
+    .patch(
+      `interview/confirmByEmployee?idEmployee=${candidateId}&idInterview=${interviewId}`, {},
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
+    .then((response) => response.data)
+    .catch((error) => error.response.data);
+};
+
+export const rejectInterview = async (token, candidateId, interviewId) => {
+  return await axiosConfig
+    .patch(
+      `interview/rejectByEmployee?idEmployee=${candidateId}&idInterview=${interviewId}`, {},
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
+    .then((response) => response.data)
+    .catch((error) => error.response.data);
 };
